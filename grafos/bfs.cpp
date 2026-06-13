ed97a250a89883a5f99186dd687b9a756e5f10e7
Arthur Ramalho <artramalho@proton.me>
2026-06-12 23:58:48 -0300

    feat: Adiciona algoritmo bfs

---
+++ grafos/bfs.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bfs (vector<vector<int>> &grafo, int origem, int n) {
+       vector<bool> visitados;
+       queue<int> fila;
+
+       visitados.assign(n+1, false);
+
+       fila.push(origem);
+       visitados[origem] = true;
+
+       while(!fila.empty()) {
+              int atual = fila.front();
+              fila.pop();
+
+              for(int vizinho : grafo[atual]) {
+                     if(!visitados[atual]) {
+                            visitados[atual] = true;
+                            fila.push(vizinho);
+                     }
+              }
+       }
+}
+
+int main () {
+       int n, m, origem, destino;
+       cin >> n >> m;
+
+       vector<vector<int>> grafo;
+       grafo.resize(n+1);
+
+       for(int i=0 ; i<m ; i++) {
+              int u, v;
+              cin >> u >> v;
+
+              grafo[u].push_back(v);
+              grafo[v].push_back(u);
+       }
+
+       bfs(grafo, origem, n);
+}
