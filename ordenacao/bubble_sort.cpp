26662be7c89c67369326a3b50de4ba435166980b
Arthur Ramalho <artramalho@proton.me>
2026-06-15 12:54:21 -0300

    feat: Adiciona bubble-sort

---
+++ ordenacao/bubble_sort.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+
+vi bubble_sort (vi &lista) {
+       bool troca;
+       for(int i = 0 ; i < lista.size()-1 ; i++) {
+              for(int j = 0 ; j < lista.size() - i - 1 ; j++) {
+
+                     if(lista[j] > lista[j+1]) {
+                            int aux  = lista[j];
+                            lista[j] = lista[j+1];
+                            lista[j+1] = aux;
+                            troca = true;
+                     }
+              }
+              if(!troca) break;
+       }
+       return lista;
+}
+
+int main () {
+       vi lista, lista_ordenada;
+       int tam;
+       cin >> tam;
+       for(int i = 0 ; i < tam ; i++) {
+              int num;
+              cin >> num;
+              lista.push_back(num);
+       }
+
+       lista_ordenada = bubble_sort(lista);
+       for(int i=0 ; i < tam ; i++) {
+              cout << lista[i] << " ";
+       }
+       cout << "\n";
+       return 0;
+}
